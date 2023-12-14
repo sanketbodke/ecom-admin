b6e3d76a86ec010819d5d6b4ca4fef1333888215
sanket bodke <sanketbodke26@gmail.com>
2023-12-14 20:25:41 +0530

    users model add

--- ecom_backend/src/models/user.model.js
+++ ecom_backend/src/models/user.model.js
@@ -42,4 +42,17 @@ const userSchema = new Schema({
   // orders:{}
 }, {
     timestamps: true,
-  });
+});
+
+userSchema.pre("save" , async function(next){
+  if(this.isModified("password")){
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+  } else
+      return next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(password, this.password)
+}
+export const User = mongoose.model("User", userSchema)
