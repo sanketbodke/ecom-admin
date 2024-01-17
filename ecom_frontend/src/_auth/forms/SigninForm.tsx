65d90efda4f49e2117ddb58bd4fe2501a5d0d383
sanket bodke <sanketbodke26@gmail.com>
2024-01-17 15:26:22 +0530

    client inital commit

--- ecom_frontend/src/_auth/forms/SigninForm.tsx
+++
@@ -1,9 +0,0 @@
-const SigninForm = () => {
-  return (
-    <div>
-      signin
-    </div>
-  )
-}
-
-export default SigninForm
--- ecom_frontend/src/lib/validation/index.ts
+++
@@ -1,8 +0,0 @@
-import * as z from "zod"
-
-export const SignupValidationSchema = z.object({
-    name: z.string().min(2, {message: "String must contain at least 2 character(s)"}),
-    username: z.string().min(2, {message: "To Short"}),
-    email: z.string().email(),
-    password: z.string().min(8, {message: "Password must be at least 8 characters"}),
-});
