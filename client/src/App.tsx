ba4926db0e9fcb2ef91685b1a8bec8d370e57a17
sanket bodke <sanketbodke26@gmail.com>
2024-01-19 00:15:06 +0530

    routes add

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,20 @@
-import { Button } from "@/components/ui/button"
-
-export default function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import authLayout from "@/pages/auth/authLayout.tsx";
+import login from "@/pages/auth/forms/login.tsx";
+import register from "@/pages/auth/forms/register.tsx";
+const App = () => {
     return (
-        <div>
-            <Button>Click me</Button>
-        </div>
-    )
-}
+        <>
+            <Router>
+                <Routes>
+                    <Route Component={authLayout}>
+                        <Route path="/login" Component={login}></Route>
+                        <Route path="/register" Component={register}></Route>
+                    </Route>
+                </Routes>
+            </Router>
+        </>
+    );
+};
+
+export default App;
