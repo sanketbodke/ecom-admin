4cac7b97171278906a2512e7e7039c1040009c96
sanket bodke <sanketbodke26@gmail.com>
2024-01-26 00:31:59 +0530

    coverImage managed by frontend

--- ecom_backend/src/controllers/billBoard.controller.js
+++ ecom_backend/src/controllers/billBoard.controller.js
@@ -2,15 +2,14 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { billBoard } from "../models/billBoard.model.js";
-import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const generateBillBoard = asyncHandler(async (req,resp)=> {
   // get data from frontend
 
-  const { label } = req.body;
+  const { label, coverImage } = req.body;
 
   if(
-    [label].some((fields)=> fields.trim() === "")
+    [label, coverImage].some((fields)=> fields.trim() === "")
   ) {
     throw new ApiError(400, "label are required")
   }
@@ -23,27 +22,11 @@ const generateBillBoard = asyncHandler(async (req,resp)=> {
     throw new ApiError(409, "Label already exist")
   }
 
-  // check cover img
-
-  const coverImageLocalPath = req.files?.coverImage?.[0]?.path
-
-  if(!coverImageLocalPath){
-    throw new ApiError(400, "CoverImage Required")
-  }
-
-  // upload on cloudinary
-
-  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
-
-  if(!coverImage){
-    throw new ApiError(400, "coverImage is required")
-  }
-
   // store on database
 
   const createdBillBoard = await billBoard.create({
-    coverImage: coverImage.url,
     label,
+    coverImage
   })
 
   // return response
--- ecom_backend/src/routes/billBoard.route.js
+++ ecom_backend/src/routes/billBoard.route.js
@@ -1,16 +1,9 @@
 import { Router } from "express";
 import { generateBillBoard, getAllBillBoards } from "../controllers/billBoard.controller.js";
-import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router()
 
 router.route("/create").post(
-  upload.fields([
-    {
-      name: "coverImage",
-      maxCount: 1,
-    },
-  ]),
   generateBillBoard
 )
 
