6cc99191cf27635b2edb7b8a1ef863fe44065719
sanket bodke <sanketbodke26@gmail.com>
2024-02-06 15:03:51 +0530

    routes ad

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -12,18 +12,22 @@ import update from "@/pages/billBoard/update.tsx";
 // categories
 import categories from "@/pages/categories/categories.tsx";
 import createCategories from "@/pages/categories/create.tsx"
+import updateCategories from "@/pages/categories/update.tsx"
 
 // size
 import sizes from "@/pages/size/sizes.tsx";
 import createSize from "@/pages/size/create.tsx";
+import updateSize from "@/pages/size/update.tsx";
 
 // color
 import colors from "@/pages/color/Colors.tsx";
 import createColor from "@/pages/color/create.tsx";
+import updateColor from "@/pages/color/update.tsx";
 
 // products
 import product from "@/pages/product/product.tsx";
 import createProduct from "@/pages/product/create.tsx"
+import updateProduct from "@/pages/product/update.tsx"
 
 const App = () => {
     return (
@@ -56,15 +60,19 @@ function AppRouter(){
 
                     <Route path="/categories" Component={categories}></Route>
                     <Route path="/categories/create" Component={createCategories}></Route>
+                    <Route path="/categories/:id/update" Component={updateCategories}></Route>
 
                     <Route path="/sizes" Component={sizes}></Route>
                     <Route path="/sizes/create" Component={createSize}></Route>
+                    <Route path="/sizes/:id/update" Component={updateSize}></Route>
 
                     <Route path="/colors" Component={colors}></Route>
                     <Route path="/color/create" Component={createColor}></Route>
+                    <Route path="/colors/:id/update" Component={updateColor}></Route>
 
                     <Route path="/products" Component={product}></Route>
                     <Route path="/product/create" Component={createProduct}></Route>
+                    <Route path="/products/:id/update" Component={updateProduct}></Route>
                 </Route>
             </Routes>
         </>
