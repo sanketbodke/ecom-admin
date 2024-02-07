85354defc32bac9d4a59916248e9c9f2dbd3e6ca
sanket bodke <sanketbodke26@gmail.com>
2024-02-07 22:25:53 +0530

    categories add

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
+import Navbar from "./components/Navbar.tsx";
+import Footer from "./components/Footer.tsx";
+import Home from "./pages/home.tsx";
 function App() {
   return (
     <>
-
+      <Navbar />
+      <Home />
+      <Footer />
     </>
   )
 }
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
-
+import React, {useEffect, useState} from 'react';
+import axios from "axios"
+import API_BASE_URL from "../constant.ts";
+import {Link} from "react-router-dom";
 const Navbar: React.FC = () => {
+    const [categories, setCategories] = useState({})
+    useEffect(() => {
+        const getCategories = async () => {
+            try{
+                const response = await axios.get(`${API_BASE_URL}/api/v1/categories`)
+                setCategories(response.data.data)
+            }catch (error){
+                console.log(error)
+            }
+        }
+        getCategories();
+    }, []);
     return (
         <nav className="border flex justify-between items-center p-4">
             <div className="flex justify-between items-center text-xl">
                 <h1 className="mr-6">STORE</h1>
                 <div className="flex justify-between items-center gap-4 text-sm">
-                    <p>Shirts</p>
-                    <p>Shoes</p>
+                    {categories.length > 0 ? (
+                        categories.map((category, key) => (
+                            <p key={key}>{category.name}</p>
+                        ))
+                    ) : (
+                        <p>No Categories</p>
+                    )}
                 </div>
             </div>
             <div className="flex justify-between items-center gap-4 text-sm">
