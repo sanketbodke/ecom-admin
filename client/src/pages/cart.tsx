c033491f7c1bd83e735480bbbdb1b779a22e07c0
sanket bodke <sanketbodke26@gmail.com>
2024-02-19 16:30:43 +0530

    add to cart api add

--- client/src/pages/cart.tsx
+++ client/src/pages/cart.tsx
@@ -2,50 +2,54 @@ import React, { useEffect, useState } from 'react';
 import { useSelector } from "react-redux";
 import axios from "axios";
 import API_BASE_URL from "../constant.ts";
-import {Link} from "react-router-dom"
 import {Button} from "../components/ui/button.tsx";
 
 const Cart: React.FC = () => {
     const { currentUser } = useSelector((state) => state.user);
+    const userId = currentUser?.data?.data?.user?._id || ""
     const [cartProducts, setCartProducts] = useState([]);
-
+    const subtotal = cartProducts.reduce((accumulator, product) => accumulator + parseFloat(product.price), 0);
     useEffect(() => {
         const getProducts = async () => {
             try {
-                const cart = currentUser?.data?.data?.user?.cart || [];
-                cart.map(async (id: number) => {
-                    const response = await axios.get(`${API_BASE_URL}/api/v1/products/${id}`);
-                    setCartProducts(response.data.data)
-                });
+                const cartProducts = await axios.get(`${API_BASE_URL}/api/v1/products/cart/${userId}`)
+                setCartProducts(cartProducts.data.data)
             } catch (error) {
                 console.error('Error fetching cart products:', error);
             }
         };
         getProducts();
-    }, [currentUser]);
+    }, []);
 
     return (
-        <div className={"flex justify-between text-sm p-4"}>
-            <div className={"flex gap-4"}>
-                <img
-                    src={cartProducts.productImage}
-                    alt={"Product img"}
-                    className={"w-36 h-32 object-cover bg-center"}
-                />
-                <div>
-                    <p>Name : {cartProducts.name}</p>
-                    <p>Size : {cartProducts.size}</p>
-                    <p>Color : {cartProducts.color}</p>
-                    <p>Price: &#8377; {cartProducts.price}</p>
-                    <p className={"text-green-800 cursor-pointer"}>Delete</p>
-                </div>
+        <div className={"flex justify-between p-4 text-sm"}>
+            <div className={"flex flex-col gap-6 basis-[75%]"}>
+                {cartProducts.map((product, index) => (
+                    <div key={index} className={"flex items-center gap-4 border basis-3/5"}>
+                        <img
+                            src={product.productImage}
+                            alt={"Product img"}
+                            className={"w-36 h-32 object-cover bg-center"}
+                        />
+                        <div>
+                            <p>Name : {product.name}</p>
+                            <p>Size : {product.size}</p>
+                            <p>Color : {product.color}</p>
+                            <p>Price: &#8377; {product.price}</p>
+                            <p className={"text-green-800 cursor-pointer"}>Delete</p>
+                        </div>
+                    </div>
+                ))}
             </div>
-            <div className={"border w-80 p-2"}>
-                <p>Subtotal (5 items): RS 34,860.00</p>
-                <Button className={"mt-4"}>Proceed to Buy</Button>
+            <div className={"border w-80 h-fit p-2 basis-[23%]"}>
+                <p>Subtotal ({cartProducts.length} item{cartProducts.length !== 1 ? 's' : ''}) &#8377;{subtotal}</p>
+                <Button
+                    className={"mt-4"}
+                >Proceed to Buy</Button>
             </div>
         </div>
     );
+
 };
 
 export default Cart;
